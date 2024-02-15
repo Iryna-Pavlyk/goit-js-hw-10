63cbc5a61f735ce5a0f945deae6f75456393b5a7
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-02-15 12:47:37 +0000

    Deploying to gh-pages from @ Iryna-Pavlyk/goit-js-hw-10@010563561d54976bdf6734ecce783dc2a94454f8 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/styles-35483221.js";import{f as t}from"./assets/vendor-4daf66c6.js";const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t("#datetime-picker",o);
+//# sourceMappingURL=commonHelpers.js.map
